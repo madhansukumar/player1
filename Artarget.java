09895c14adcebffc01faa45e26507af2d7217bd6
madhansukumar <madhankumarsrv@gmail.com>
2017-06-09 15:41:54 +0530

    Create Artarget.java

---
+++ Artarget.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Artarget {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n;
+        n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        int tar=s.nextInt();
+        for(int i=0;i<n;i++)
+                {
+                    for(int j=i+1;j<n;j++)
+                    {
+                        if(a[i]+a[j]==tar)
+                        {
+                            System.out.print(+a[i]+" "+a[j]);
+                        }
+                    }
+                }
